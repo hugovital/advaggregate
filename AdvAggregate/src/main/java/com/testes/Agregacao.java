3f2bc1907b057f14037e3953d08ebc2c36b1bd6f
root <root@new_dragon.localdomain>
2018-03-26 03:13:27 +0000

    versao com retry em repository (work in progress)

--- AdvAggregate/src/main/java/com/testes/Agregacao.java
+++ AdvAggregate/src/main/java/com/testes/Agregacao.java
@@ -17,12 +17,23 @@ public class Agregacao implements AggregationStrategy, TimeoutAwareAggregationSt
 	@Override
 	public void timeout(Exchange oldExchange, int index, int total, long timeout) {
 		
+		oldExchange.setProperty("agregacaoDeuTimeout", true); //<--- importante marcar o timeout para controle no fluxo pó-agregação		
+			
 		System.out.println("=======> aconteceu timeout");
 		
 	}
 
 	@Override
 	public Exchange aggregate(Exchange oldExchange, Exchange newExchange) {
+		
+		if (oldExchange == null)
+			return newExchange;
+		
+		//se chegou aqui, é porque existe um oldExchange, o que quer dizer que duas mensagens foram recebidas.
+		//como no meu caso são 2 serviços que têm que aconecer, isso é um caso de sucesso
+		
+		newExchange.setProperty("agregacaoComSucesso", true);
+		
 		return newExchange;
 	}
 
--- AdvAggregate/src/main/java/com/testes/MyRoutes.java
+++ AdvAggregate/src/main/java/com/testes/MyRoutes.java
@@ -6,6 +6,7 @@ import org.apache.camel.Exchange;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.hawtdb.HawtDBAggregationRepository;
 import org.apache.camel.processor.aggregate.AggregationStrategy;
+import org.apache.camel.processor.aggregate.OptimisticLockRetryPolicy;
 
 public class MyRoutes extends RouteBuilder {
 
@@ -51,20 +52,54 @@ public class MyRoutes extends RouteBuilder {
 		from("direct:serv_02")
 			.log("inside serv 02")
 			.transform().constant("retorno do servico 02")
+			//.throwException(new Exception("hugo servico 2 exception"))
 			.to("direct:aggregate");
 
-
-		HawtDBAggregationRepository hawtDBRepo = new HawtDBAggregationRepository("repo1", "c:/users/hugo/desktop/hawtdb.dat");			
+		HawtDBAggregationRepository hawtDBRepo = new HawtDBAggregationRepository("repo1", "c:/users/hugo/desktop/hawtdb.dat");
+		OptimisticLockRetryPolicy hugoRetry = new OptimisticLockRetryPolicy();
+		hugoRetry.setRetryDelay(10 * 1000); // <- 1 sec para ficar retentando
+		hugoRetry.setExponentialBackOff(true);
+		hugoRetry.setMaximumRetries(1);
 		
 		from("direct:aggregate")
 			.aggregate( header("id") , new Agregacao() )
-			.aggregationRepository( hawtDBRepo )
+			//.aggregationRepository( hawtDBRepo )
+
+				//aqui configurações para retry quando dá erro depois que completa o aggregate (isto é, dá erro no desfazimento)
+				//.optimisticLocking()
+				//.optimisticLockRetryPolicy( hugoRetry )
+
 			.completionSize(2)
 			.completionTimeout(10000)
+			
 			.to("direct:end_aggregate");
 		
 		from("direct:end_aggregate")
+
+			.log("agregacaoComSucesso: ${property.agregacaoComSucesso}")
 			.log("Final da Agregação: ${body}");
+			
+		/*
+			.choice()
+
+				.when ( simple("${property.agregacaoComSucesso} == true") )
+					.log("Final da Agregação: ${body}")
+				
+				.when ( simple("${property.agregacaoDeuTimeout} == true") )				
+					//aqui é o caso de erro na agregação (algum serviço não respondeu, deu timeout, etc)
+					//daí abaixo seria um "to" para o serviço de desfazimento
+					//mas como eu quero forçar o retry (caso onde falha o serviço de desfazimento), tome-lhe exception				
+					//.throwException(new Exception("Hugo Exception"))
+					
+				.otherwise()
+					//aqui é quando bem completou nem deu timeout, ou seja, quando retanta. vou dar exceção para ficar em loop
+
+					//.throwException(new Exception("Hugo Exception"))
+					
+					
+			.endChoice();
+	*/
+		
 
 		from("direct:desfazer")
 			.log("dentro do desfazimento");
